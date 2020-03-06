e0f1ea98c865880255619e205adb4f66ef0b1c2e
Hangur <hangur@hangur>
2020-03-06 14:59:56 +0100

    disable server banner

--- app/server.js
+++ app/server.js
@@ -3,9 +3,12 @@ const express = require('express');
 exports.createServer = ({ config, logger }) => {
   const _express = express();
 
-  _express.locals = { config, logger };
 
-  _express.use('/health', (_, res) => res.status(200).end());
+  _express
+    .disable('x-powered-by')
+    .use('/health', (_, res) => res.status(200).end());
+
+  _express.locals = { config, logger };
 
   return _express;
 };
