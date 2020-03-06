0014f52401d4cf3fb66608ba555b7c9768ce00a1
Hangur <hangur@hangur>
2020-03-06 15:12:52 +0100

    add json middleware

--- app/server.js
+++ app/server.js
@@ -3,9 +3,9 @@ const express = require('express');
 exports.createServer = ({ config, logger }) => {
   const _express = express();
 
-
   _express
     .disable('x-powered-by')
+    .use(express.json())
     .use('/health', (_, res) => res.status(200).end())
     .use((_, res) => res.status(404).json('not found'));
 
