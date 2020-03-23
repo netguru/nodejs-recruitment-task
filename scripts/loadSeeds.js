2a980d270ced310ee40fc328b6213fca00669b64
Tomasz Nowacki <tomasz.nowacki@netguru.co>
2020-03-23 14:50:54 +0100

    add admin user to seeds

--- scripts/loadSeeds.js
+++ scripts/loadSeeds.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   username: String,
   token: String,
   permissions: [String],
+  isAdmin: Boolean,
 });
 
 const User = mongoose.model('User', userSchema);
