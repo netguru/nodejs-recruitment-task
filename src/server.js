2b832ca2b77152478e7bc5c50bb767c3151855d4
Tomasz Nowacki <tomasz.nowacki@netguru.co>
2020-12-07 11:19:43 +0100

    init

---
+++ src/server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { authFactory, AuthError } = require("./auth");
+
+const PORT = 3000;
+const { JWT_SECRET } = process.env;
+
+if (!JWT_SECRET) {
+  throw new Error("Missing JWT_SECRET env var. Set it and restart the server");
+}
+
+const auth = authFactory(JWT_SECRET);
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post("/auth", (req, res, next) => {
+  if (!req.body) {
+    return res.status(400).json({ error: "invalid payload" });
+  }
+
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ error: "invalid payload" });
+  }
+
+  try {
+    const token = auth(username, password);
+
+    return res.status(200).json({ token });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      return res.status(401).json({ error: error.message });
+    }
+
+    next(error);
+  }
+});
+
+app.use((error, _, res, __) => {
+  console.error(
+    `Error processing request ${error}. See next message for details`
+  );
+  console.error(error);
+
+  return res.status(500).json({ error: "internal server error" });
+});
+
+app.listen(PORT, () => {
+  console.log(`auth svc running at port ${PORT}`);
+});
