f960cabf7aaa34b734574eec532609b46862ad12
jBosak <jbosak98@gmail.com>
2021-01-25 19:53:03 +0100

    authorization endpoint - few tests

--- test/integration/auth.test.js
+++ test/integration/auth.test.js
@@ -1,13 +1,72 @@
 const expect = require("chai").expect;
 const server = require("../../src/server");
-const request = require("supertest")(server); // 
+const request = require("supertest")(server);
+const pick = require("lodash/pick");
+const users = require("../fixtures/users");
+const faker = require("faker");
+const errorResponses = require('../fixtures/errorResponses');
 
 describe("authorization API", () => {
+  const premiumUser = pick(users[1], ["username", "password"]);
+  const basicUser = pick(users[0], ["username", "password"]);
+  const userWithWrongPassword = {
+    username: basicUser.username,
+    password: basicUser.password + faker.random.number(),
+  };
+
   it("empty request should respond with 404 code", (done) => {
-      request.post('/api/auth').end((err,response) => {
-          expect(response.statusCode).to.equal(404);
-          expect(response.body).to.be.empty;
-      })
+    request.post("/auth").end((err, response) => {
+      expect(response.statusCode).to.equal(400);
+      expect(response.body).to.deep.equal(errorResponses.invalidPayload);
+    });
+    done();
+  });
+
+  it("basic user", (done) => {
+    request
+      .post("/auth")
+      .set("Accept", "application/json")
+      .send(basicUser)
+      .end((err, response) => {
+        expect(response.statusCode).to.equal(200);
+        expect(response.body).to.have.property("token");
+      });
+    done();
+  });
+
+  it("premium user user", (done) => {
+    request
+      .post("/auth")
+      .set("Accept", "application/json")
+      .send(premiumUser)
+      .end((err, response) => {
+        expect(response.statusCode).to.equal(200);
+        expect(response.body).to.have.property("token");
+      });
+    done();
+  });
+
+  it("wrong password", (done) => {
+    request
+      .post("/auth")
+      .set("Accept", "application/json")
+      .send(userWithWrongPassword)
+      .end((err, response) => {
+        expect(response.statusCode).to.equal(401);
+        expect(response.body).to.deep.equal(errorResponses.invalidCredentials);
+      });
+    done();
+  });
+
+  it("empty credentials", (done) => {
+    request
+      .post("/auth")
+      .set("Accept", "application/json")
+      .send({ username: "", password: "" })
+      .end((err, response) => {
+        expect(response.statusCode).to.equal(400);
+        expect(response.body).to.deep.equal(errorResponses.invalidPayload);
+      });
     done();
   });
 });
