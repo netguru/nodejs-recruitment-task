baa87b1c99729578328908f75389b1a8e9a67e45
jBosak <jbosak98@gmail.com>
2021-01-25 19:52:28 +0100

    fixtures - users and errorResponses

---
+++ test/fixtures/errorResponses.js
@@ -0,0 +1,7 @@
+const errorResponses = {
+  invalidPayload: { error: "invalid payload" },
+  internalError: { error: "internal server error" },
+  invalidCredentials: { error: "invalid username or password" },
+};
+
+module.exports = errorResponses;
---
+++ test/fixtures/users.js
@@ -0,0 +1,18 @@
+const users = [
+  {
+    id: 123,
+    role: "basic",
+    name: "Basic Thomas",
+    username: "basic-thomas",
+    password: "sR-_pcoow-27-6PAwCD8",
+  },
+  {
+    id: 434,
+    role: "premium",
+    name: "Premium Jim",
+    username: "premium-jim",
+    password: "GBLtTyq3E_UNjFnpo9m6",
+  },
+];
+
+module.exports = users;
