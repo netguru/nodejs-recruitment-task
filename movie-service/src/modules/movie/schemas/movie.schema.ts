a826df6a5d5cf1cf98439b0e108bc4c8f848e129
David Solomon <dav.soli22@gmail.com>
2021-03-04 09:18:41 +0100

    Create movie schema

--- movie-service/src/modules/movie/schemas/movie.schema.ts
+++ movie-service/src/modules/movie/schemas/movie.schema.ts
@@ -10,6 +10,37 @@ export class Movie extends Document {
     type: String,
     required: true,
   })
+  userId: string;
+
+  @Prop({
+    type: String,
+    required: true,
+  })
   title: string;
+
+  @Prop({
+    type: Date,
+    required: true,
+  })
+  released: Date;
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  genre: string;
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  director: string;
+
+  @Prop({
+    type: Date,
+    required: true,
+    default: () => Date.now(),
+  })
+  createdAt: Date;
 }
 export const MovieSchema = SchemaFactory.createForClass(Movie);
