f74265bfd969158a187ffaac88c4b1f5cae1a9f4
Hangur <hangur@hangur>
2021-06-10 17:34:12 +0200

    Small fix to handle missing released date

--- movies/src/db/migrations/20210608201716_init.mjs
+++ movies/src/db/migrations/20210608201716_init.mjs
@@ -1,16 +1,16 @@
 const up = (knex) => {
-  console.log("Executing migration");
+  console.log("Executing movies migration");
   return knex.schema.createTable("movies", table => {
       table.increments("id");
       table.string("title").notNullable().unique();
-      table.datetime("released").notNullable();
+      table.string("released").notNullable();
       table.string("genre").notNullable();
       table.string("director").notNullable();
   });
 };
 
 const down = (knex) => {
-  console.log("Reverting migration");
+  console.log("Reverting movies migration");
   return knex.schema.dropTable("movies");
 };
 
