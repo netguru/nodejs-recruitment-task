c497dd07308319712b93fa3aebe8aa5396b57c8b
Krzysztof Sobol <krzysieksobol93@gmail.com>
2021-08-09 20:46:06 +0200

    MoviesService.ts
    1. 'create()' funtcion parameters improved

--- services/movies/src/components/movie/moviesService.ts
+++ services/movies/src/components/movie/moviesService.ts
@@ -1,16 +1,22 @@
+import { UserJWT } from '../../../../../shared/src/interfaces/User';
 import { Movie } from '../../../../../shared/src/models/Movie';
 import * as moviesRepository from './moviesRepository';
 
-export const getAll = async (): Promise<Movie[]> => {
-  return moviesRepository.getAll();
-};
-
-export const create = async (title: string): Promise<Movie> => {
+function fetchData(title: string): Movie {
   const movie = new Movie();
   movie.title = title;
   movie.released = '25 Jun 1982';
   movie.genre = 'Action';
   movie.director = 'Spielberg';
   movie.userId = 1;
+  return movie;
+}
+
+export const getAll = async (): Promise<Movie[]> => {
+  return moviesRepository.getAll();
+};
+
+export const create = async (user: UserJWT, title: string): Promise<Movie> => {
+  const movie = fetchData(title);
   return moviesRepository.create(movie);
 };
