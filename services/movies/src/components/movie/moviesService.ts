6c56d1b43a6a7018b46e94f8fdd642044c1e59bd
Krzysztof Sobol <krzysieksobol93@gmail.com>
2021-08-10 22:43:00 +0200

    Error handling
    1. Added throwing 'BadRequestError' when no title provided

--- services/movies/src/components/movie/moviesService.ts
+++ services/movies/src/components/movie/moviesService.ts
@@ -1,6 +1,6 @@
 import { UserJWT } from '../../../../../shared/src/interfaces/User';
 import { Movie } from '../../../../../shared/src/models/Movie';
-import { ForbiddenError } from '../../../../../shared/src/utils/errors';
+import { BadRequestError, ForbiddenError } from '../../../../../shared/src/utils/errors';
 import * as moviesRepository from './moviesRepository';
 import * as omdb from '../../../../../shared/src/providers/omdb';
 
@@ -9,6 +9,10 @@ export const getByUserId = async (userId: number): Promise<Movie[]> => {
 };
 
 export const create = async (user: UserJWT, title: string, year: number): Promise<Movie> => {
+  if (!title) {
+    throw new BadRequestError('Invalid payload');
+  }
+
   if (user.role.toLowerCase() === 'basic') {
     const count = await moviesRepository.countByUserIdCurMonth(user.userId);
     if (count > 4) {
