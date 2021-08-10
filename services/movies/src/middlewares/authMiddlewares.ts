2a60ca7c4679178031efd879b864035f7f5fbbff
Krzysztof Sobol <krzysieksobol93@gmail.com>
2021-08-11 01:50:58 +0200

    AuthMiddleware.ts
    1. Removed default export
    2. Added 'verify()' function

--- services/movies/src/middlewares/authMiddlewares.ts
+++ services/movies/src/middlewares/authMiddlewares.ts
@@ -1,20 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 
-import { UserJWT } from '../../../../shared/src/interfaces/User';
 import { ForbiddenError, UnAuthorizedError } from '../../../../shared/src/utils/errors';
 
-export default (req: Request, res: Response, next: NextFunction): void => {
+export function verify(req: Request, res: Response, next: NextFunction): void {
   const authHeader = req.headers.authorization;
   const token = authHeader?.split(' ')[1];
 
-  if (!token) throw new UnAuthorizedError('Missing token');
-  else
-    jwt.verify(token, process.env.JWT_SECRET, (err: jwt.VerifyErrors, user: UserJWT): void => {
-      if (err) throw new ForbiddenError('Bad token');
-      else {
-        req.body.user = user;
-        next();
-      }
-    });
-};
+  if (!token) {
+    throw new UnAuthorizedError('Missing token');
+  }
+
+  try {
+    req.body.user = jwt.verify(token, process.env.JWT_SECRET);
+    next();
+  } catch {
+    throw new ForbiddenError('Bad token');
+  }
+}
--- services/movies/src/providers/routes.ts
+++ services/movies/src/providers/routes.ts
@@ -1,7 +1,7 @@
 import { Express, Request, Response } from 'express';
 
 import moviesRouter from '../components/movie/moviesRouter';
-import jwtAuth from '../middlewares/authMiddlewares';
+import * as authMiddlewares from '../middlewares/authMiddlewares';
 
 const healthCheckHandler = (req: Request, res: Response): void => {
   res.json({ message: 'Health check - status ok!' });
@@ -10,5 +10,5 @@ const healthCheckHandler = (req: Request, res: Response): void => {
 export const mountApi = (app: Express): void => {
   app.get('/', healthCheckHandler);
 
-  app.use('/movies', jwtAuth, moviesRouter);
+  app.use('/movies', authMiddlewares.verify, moviesRouter);
 };
