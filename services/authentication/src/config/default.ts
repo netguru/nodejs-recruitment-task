dccd95f2a7198a1cbfccebcf208f777c7f6b6e5d
Krzysztof Sobol <krzysieksobol93@gmail.com>
2021-08-10 23:30:30 +0200

    Environmental variables
    1. Added check for environmental variables existing (both services)
    2. Improved container start command (both services)

--- services/authentication/src/config/default.ts
+++ services/authentication/src/config/default.ts
@@ -1,3 +1,11 @@
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+const environmentalVariables = ['NODE_ENV', 'PORT', 'JWT_SECRET'];
+
+environmentalVariables.forEach((name: string) => {
+  if (!process.env[name]) {
+    throw new Error(`Missing ${name} environmental variable`);
+  }
+});
--- services/movies/src/config/default.ts
+++ services/movies/src/config/default.ts
@@ -1,3 +1,22 @@
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+const environmentalVariables = [
+  'NODE_ENV',
+  'PORT',
+  'DB_HOST',
+  'DB_PORT',
+  'DB_USER',
+  'DB_PASS',
+  'DB_NAME',
+  'JWT_SECRET',
+  'OMDB_API_URL',
+  'OMDB_API_KEY',
+];
+
+environmentalVariables.forEach((name: string) => {
+  if (!process.env[name]) {
+    throw new Error(`Missing ${name} environmental variable`);
+  }
+});
