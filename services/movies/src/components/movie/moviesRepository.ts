c38910aad6c94875acf0a8af9be3b325c9f0f70a
Krzysztof Sobol <krzysieksobol93@gmail.com>
2021-08-10 02:42:39 +0200

    Movies API
    1. Added check for basic accont of 5 records for month

--- services/movies/src/components/movie/moviesRepository.ts
+++ services/movies/src/components/movie/moviesRepository.ts
@@ -1,8 +1,20 @@
-import { getRepository } from 'typeorm';
+import { getRepository, MoreThanOrEqual } from 'typeorm';
 import { Movie } from '../../../../../shared/src/models/Movie';
 
 export const getByUserId = async (userId: number): Promise<Movie[]> => {
   return getRepository(Movie).find({ userId });
+};
+
+export const countByUserIdCurMonth = async (userId: number): Promise<number> => {
+  const year = new Date().getFullYear();
+  const month = new Date().getMonth();
+
+  const where = {
+    createdAt: MoreThanOrEqual(`${year}-${month + 1}-01`),
+    userId,
+  };
+
+  return getRepository(Movie).count({ where });
 };
 
 export const save = async (movie: Movie): Promise<Movie> => {
--- services/movies/src/components/movie/moviesService.ts
+++ services/movies/src/components/movie/moviesService.ts
@@ -1,6 +1,7 @@
 import { UserJWT } from '../../../../../shared/src/interfaces/User';
 import { Movie } from '../../../../../shared/src/models/Movie';
 import { fetchMovieDetails } from '../../../../../shared/src/providers/ombd';
+import { ForbiddenError } from '../../../../../shared/src/utils/errors';
 import * as moviesRepository from './moviesRepository';
 
 export const getByUserId = async (userId: number): Promise<Movie[]> => {
@@ -8,6 +9,13 @@ export const getByUserId = async (userId: number): Promise<Movie[]> => {
 };
 
 export const create = async (user: UserJWT, title: string, year: number): Promise<Movie> => {
+  if (user.role.toLowerCase() === 'basic') {
+    const count = await moviesRepository.countByUserIdCurMonth(user.userId);
+    if (count > 4) {
+      throw new ForbiddenError('You exceeded your monthly amount of 5 records for basic account');
+    }
+  }
+
   const movie = await fetchMovieDetails(title, year);
   movie.userId = user.userId;
 
