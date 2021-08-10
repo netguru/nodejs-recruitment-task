81cbe08fee65fe7c1ba85e55cdeaa14725af3c9a
Krzysztof Sobol <krzysieksobol93@gmail.com>
2021-08-10 22:41:55 +0200

    Reformat
    1. Removed '.' from error message

--- services/movies/src/app/exceptionHandlers.ts
+++ services/movies/src/app/exceptionHandlers.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 import { APIError, HttpStatusCode, NotFoundError } from '../../../../shared/src/utils/errors';
 
 export const notFoundErrorHandler = (): void => {
-  throw new NotFoundError('Endpoint not found.');
+  throw new NotFoundError('Endpoint not found');
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
