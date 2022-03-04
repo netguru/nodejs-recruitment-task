8bea942e0913efcac082ee638e1f41e1ed96c1f1
anthugny <tooneynta92@gmail.com>
2022-03-04 14:56:17 +0100

    Update server.ts

--- movie-service/src/server.ts
+++ movie-service/src/server.ts
@@ -6,8 +6,8 @@ mongoose.connect(process.env.databaseUrl);
 const db = mongoose.connection;
 db.on("open", ()=>{
     console.log("mongodb connected");
-})
+});
 
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`);
-})
+});
