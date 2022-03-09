7347f6c1e6df126038a9691115338790acc995e1
Nurudeen Adewale <nurudeenadeolaa@gmail.com>
2022-03-09 21:51:33 +0100

    Added test for get request

---
+++ src/tests/movies.test.js
@@ -0,0 +1,16 @@
+const supertest = require('supertest')
+const { app } = require('../server')
+const requestWithSupertest = supertest(app);
+const { Movies } = require("../models/movies")
+
+describe('Movies Endpoints', () => {
+    it('GET /movies should get movies details', async () => {
+        const movies = await Movies.find();
+        const res = await requestWithSupertest.get('/movies');
+        expect(res.status).toEqual(200);
+        expect(res.type).toEqual(expect.stringContaining('json'));
+        expect(JSON.parse(res.text).data[0].title).toBe(movies[0].title);
+        expect(JSON.parse(res.text).data[0].genre).toBe(movies[0].genre);
+    });
+    
+});
