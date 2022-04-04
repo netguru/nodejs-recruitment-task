1668fa671d2d0478ad240b5d7166a7114c2487e2
Fawas Kareem <phawazzzy@gmail.com>
2022-04-04 01:44:34 +0100

    removed console log

--- services/movies/src/repository/movies.repository.js
+++ services/movies/src/repository/movies.repository.js
@@ -34,7 +34,6 @@ class MoviesRepository {
             const res = await Movies.create(data);
             return res;
         } catch (error) {
-            console.log(error);
             throw new Error(error);
         }
     }
